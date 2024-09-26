cd34e213ceca877aede6e4fe47a35bb5d01e55f3
JuanM0412 <juangomezp1204@gmail.com>
2024-09-26 10:41:03 -0500

    Third algorithm for local search

--- Neighborhood_Search/change_position_different_routes.py
+++ Neighborhood_Search/change_position_different_routes.py
@@ -3,9 +3,8 @@ import os
 from copy import deepcopy
 from output import save_results_to_excel
 
-
-OUTPUT_FILE = 'Busqueda_En_Vecindarios/output/VRPTW_JuanManuelGomez_Vecindario2.xlsx'
-INSTANCES_DIR = 'test'
+OUTPUT_FILE = 'Neighborhood_Search/output/VRPTW_JuanManuelGomez_Vecindario3.xlsx'
+INSTANCES_DIR = 'instances'
 
 
 def check_solution(graph, total_distance, new_route, max_vehicle_capacity, original_route):
@@ -63,44 +62,52 @@ def check_solution(graph, total_distance, new_route, max_vehicle_capacity, origi
 
 
 def change_position(graph, vehicles, total_distance, computation_time, routes, max_vehicle_capacity):
-    for idx, route in enumerate(routes):
-        # Convertir la tupla en una lista para modificarla temporalmente
-        route_list = list(route)
-        nodes_traveled_copy = deepcopy(route_list[0])
-        current_route_distance = route_list[4]
-        travel_times = route_list[3]
-        flag = True
+    for idx1, route1 in enumerate(routes):
+        route_list1 = list(route1)
+        nodes_traveled_copy1 = deepcopy(route_list1[0])
+        current_route_distance1 = route_list1[4]
+        
+        for idx2 in range(idx1 + 1, len(routes)):
+            route_list2 = list(routes[idx2])
+            nodes_traveled_copy2 = deepcopy(route_list2[0])
+            current_route_distance2 = route_list2[4]
+            flag = True
 
-        i = 1
-        while i < len(nodes_traveled_copy) - 1:
-            j = i + 1
-            while j < len(nodes_traveled_copy) - 1:
-                # Intercambiar las posiciones de los nodos
-                nodes_traveled_copy[i:j+1] = nodes_traveled_copy[i:j+1][::-1]
-                #print('Traveled nodes[i:j]', nodes_traveled_copy)
-                
-                # Verificar factibilidad y distancia
-                factibility, new_total_distance = check_solution(deepcopy(graph), deepcopy(total_distance), nodes_traveled_copy, deepcopy(max_vehicle_capacity), route)
-                
-                if factibility and new_total_distance < current_route_distance:
-                    flag = False
-                    print('Old distance', current_route_distance)
-                    current_route_distance = new_total_distance
-                    route_list[4] = current_route_distance  # Modificar la distancia
-                    route_list[0] = nodes_traveled_copy  # Actualizar la ruta
-                    print('NEW DISTANCE', current_route_distance)
-                    i = 1  # Reiniciar el ciclo para buscar más mejoras
+            for i in range(1, len(nodes_traveled_copy1) - 1):
+                for j in range(1, len(nodes_traveled_copy2) - 1):
+                    # Intercambiar nodos entre rutas
+                    nodes_traveled_copy1[i], nodes_traveled_copy2[j] = nodes_traveled_copy2[j], nodes_traveled_copy1[i]
+
+                    # Verificar factibilidad y distancia para ambas rutas
+                    factibility1, new_total_distance1 = check_solution(deepcopy(graph), deepcopy(total_distance), nodes_traveled_copy1, deepcopy(max_vehicle_capacity), route1)
+                    factibility2, new_total_distance2 = check_solution(deepcopy(graph), deepcopy(total_distance), nodes_traveled_copy2, deepcopy(max_vehicle_capacity), route_list2)
+
+                    if factibility1 and factibility2:
+                        new_total_distance = new_total_distance1 + new_total_distance2
+                        if new_total_distance < (current_route_distance1 + current_route_distance2):
+                            flag = False
+                            print(f'Intercambio entre rutas {idx1} y {idx2}')
+                            print(f'DISTANCIAS ANTERIORES: Ruta {idx1}: {current_route_distance1}, Ruta {idx2}: {current_route_distance2}')
+                            current_route_distance1 = new_total_distance1
+                            current_route_distance2 = new_total_distance2
+                            route_list1[4] = current_route_distance1  # Modificar la distancia en la ruta 1
+                            route_list2[4] = current_route_distance2  # Modificar la distancia en la ruta 2
+                            route_list1[0] = nodes_traveled_copy1  # Actualizar la ruta 1
+                            route_list2[0] = nodes_traveled_copy2  # Actualizar la ruta 2
+                            print(f'NUEVAS DISTANCIAS: Ruta {idx1}: {current_route_distance1}, Ruta {idx2}: {current_route_distance2}')
+                            break
+
+                    # Revertir si no es factible
+                    nodes_traveled_copy1[i], nodes_traveled_copy2[j] = nodes_traveled_copy2[j], nodes_traveled_copy1[i]
+
+                if not flag:
                     break
 
-                j += 1
+            # Actualizar las rutas después del intercambio
+            routes[idx1] = tuple(route_list1)
+            routes[idx2] = tuple(route_list2)
 
-                if flag:
-                    nodes_traveled_copy = deepcopy(route_list[0])
-            i += 1
-
-        # Convertir la lista nuevamente a una tupla después de las modificaciones
-        routes[idx] = tuple(route_list)
-
+        # Recalcular la distancia total
         total_distance = sum(route[4] for route in routes)
 
     return len(routes), total_distance, computation_time, routes, max_vehicle_capacity
